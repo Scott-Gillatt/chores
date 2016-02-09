91a8cbefc5ed42d492e3de4cca6f0691299e2ce6
Scott Gillatt <scottgillatt@gmail.com>
2016-02-08 21:02:18 -0700

    first

---
+++ app/components/chores/chores-controller.js
@@ -0,0 +1,56 @@
+app.controller('choresController', function ($scope) {
+    $scope.test = "This is a test";
+
+    $scope.Chores = [{
+        task: 'Walk dog',
+        dueDate: Date.now(),
+        responsibleParty: 'Scott',
+        estTime: 15
+        },
+        {
+            task: 'Run dog',
+            dueDate: Date.now(),
+            responsibleParty: 'Dave',
+            estTime: 30
+        },
+        {
+            task: 'Landury',
+            dueDate: Date.now(),
+            responsibleParty: 'Scott',
+            estTime: 25
+        },
+        {
+            task: 'Clean Dishs',
+            dueDate: Date.now(),
+            responsibleParty: 'Scott',
+            estTime: 20
+        },
+        {
+            task: 'Vacumm',
+            dueDate: Date.now(),
+            responsibleParty: 'Scott',
+            estTime: 45
+        },
+        {
+            task: 'Sweep',
+            dueDate: Date.now(),
+            responsibleParty: 'Scott',
+            estTime: 10
+        },
+    ]
+
+    $scope.addNewChore = function () {
+        $scope.Chores.push($scope.newChore);
+        $scope.newChore = "";
+        
+        // $scope.Chores.push({
+        //     task: $scope.newChore.task,
+        //     dueDate: $scope.newChore.dueDate,
+        //     responsibleParty: $scope.newChore.responsibleParty,
+        //     estTime: $scope.newChore.estTime
+        }
+        
+     $scope.deleteChore = function(i){
+         $scope.Chores.splice(i,1);
+     }
+    })
